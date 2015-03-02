33464b354bf837d800f101d68cf759cc1c099370
Happy-Ferret <MarkBauer@gmx.ch>
2015-03-02 04:26:53 +0100

    Added PHP5 GD dependency.

--- install-server.sh
+++ install-server.sh
@@ -8,7 +8,8 @@ sudo echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.
 
 #Update apt-get cache and install basic LAMP stack
 #as well as PHP5 Curl extension
-#(required for third party social login options).
+#(required for third party social login options)
+# and PHP5 GD extension (required for certain graphics operations)
 sudo apt-get update
-sudo apt-get install lamp-server^ php5-curl
+sudo apt-get install lamp-server^ php5-curl php5-gd
 sudo service apache2 restart 
